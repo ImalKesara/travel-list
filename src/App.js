57a7426a45196dfe13e006ebde0a97e82bef1489
LoneWolf <imalkesara2001@gmail.com>
2024-04-09 21:23:05 +0530

    added item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -20,8 +26,34 @@ const Form = () => {
   );
 };
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span
+        style={
+          item.packed
+            ? { textDecoration: "line-through" }
+            : { textDecoration: "none" }
+        }
+      >
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
 const Stats = () => {
   return (
     <footer className="stats">
